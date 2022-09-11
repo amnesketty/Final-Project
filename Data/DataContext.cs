79cd11918f507344168e3e02eaeb18a12d352098
Hafidz NA <hafidznaufalaryan@gmail.com>
2022-09-11 14:56:23 +0700

    addDataContext

---
+++ Data/DataContext.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using lounga.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace lounga.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Hotel> Characters {get; set;}
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using lounga.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<DataContext>(options => 
+    //options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql("Host=ec2-34-247-72-29.eu-west-1.compute.amazonaws.com;Database=d4pvpj44rchrgm;Username=tjyggitmlrcyvx;Password=c543c1b9affddc1bfa18e4a47c4d520265e503a4dee3f9f38829ce971ba07cb4"));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
