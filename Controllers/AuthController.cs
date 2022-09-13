fe0849d8c70b839276b2fda41851ec160991eb61
ndthaaa <ndithaa@gmail.com>
2022-09-13 20:07:46 +0700

    addRegister

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -2,11 +2,34 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using lounga.Dto.User;
+using lounga.Model;
+using Lounga.Services.AuthServices;
+using Microsoft.AspNetCore.Mvc;
 
-namespace Lounga.Controllers
+namespace lounga.Controllers
 {
-    public class AuthController
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
     {
-        
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<ServiceResponse<int>>> Register (UserRegisterDto UserRegisterDto)
+        {
+            var response = await _authService.Register(
+                new User {Username = UserRegisterDto.Username}, UserRegisterDto.Password
+            );
+            if (response.Success == true)
+            {
+                return Ok(response);
+            };
+            return BadRequest(response);
+        }
     }
 }
