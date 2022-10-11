320de0ce53cccf988be7eedb3cb11874068342d8
salsabilaamnes <amnesketty@gmail.com>
2022-10-11 09:45:46 +0700

    addDetailHotelView

--- Controllers/ViewHotelsController.cs
+++ Controllers/ViewHotelsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using lounga.Data;
 using lounga.Dto.Hotels;
 using lounga.Services.BookingHotelServices;
 using lounga.Services.FacilitiesHotelServices;
@@ -10,6 +11,7 @@ using lounga.Services.HotelServices;
 using lounga.Services.RoomServices;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 
 namespace lounga.Controllers
 {
@@ -19,12 +21,14 @@ namespace lounga.Controllers
         private readonly ILogger<ViewHotelsController> _logger;
         private readonly IHotelService _hotelService;
         private readonly IFindHotelService _findHotelService;
+        private readonly DataContext _context;
 
-        public ViewHotelsController(ILogger<ViewHotelsController> logger, IHotelService hotelService, IFindHotelService findHotelService)
+        public ViewHotelsController(ILogger<ViewHotelsController> logger, IHotelService hotelService, IFindHotelService findHotelService, DataContext context)
         {
             _logger = logger;
             _hotelService = hotelService;
             _findHotelService = findHotelService;
+            _context = context;
         }
 
         public async Task<IActionResult> GetHotel(int id)
@@ -39,6 +43,14 @@ namespace lounga.Controllers
             var response = await _findHotelService.FindHotel(searchHotelDto);
             List<FindHotelDto> findHotelDto = response.Data;
             return View(findHotelDto);
+        }
+
+        public async Task<IActionResult> DetailHotel(int id)
+        {
+            var response = await _hotelService.GetHotelById(id);
+            GetHotelDto hotelDto = response.Data;
+            return View(hotelDto);
+
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
