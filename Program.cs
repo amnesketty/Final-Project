25bfc5305590564caf4b4f74c761f751542238fe
Hafidz NA <hafidznaufalaryan@gmail.com>
2022-10-11 11:24:21 +0700

    editLayout

--- Program.cs
+++ Program.cs
@@ -107,6 +107,6 @@ app.MapControllers();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=ViewUsers}/{action=Login}/{id?}");
 
 app.Run();
