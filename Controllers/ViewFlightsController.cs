6f1ce5880beb92450e717c64e6f228bc66a694b7
ndthaaa <ndithaa@gmail.com>
2022-10-11 10:39:38 +0700

    ListFlights

--- Controllers/ViewFlightsController.cs
+++ Controllers/ViewFlightsController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using lounga.Dto.Flight;
+using lounga.Model;
 using lounga.Services.FacilitiesFlightService;
 using lounga.Services.FlightService;
 using Microsoft.AspNetCore.Mvc;
@@ -17,43 +18,38 @@ namespace lounga.Controllers
         private readonly ILogger<ViewFlightsController> _logger;
         private readonly IFacilitiesFlightService _facilitiesFlightService;
         private readonly IFlightService _flightService;
+   
        
         public ViewFlightsController(ILogger<ViewFlightsController> logger, IFlightService flightService, IFacilitiesFlightService facilitiesFlightService)
         {
             _logger = logger;
             _facilitiesFlightService = facilitiesFlightService;
             _flightService = flightService;
+          ;
         }
 
         public async Task<IActionResult> GetAllFlight() 
         {
-            // return Ok(await _flightService.GetAllFlight());
             var response = await _flightService.GetAllFlight();
             List<GetFlightDto> getFlightDtos = response.Data;
             return View(getFlightDtos);
         }
 
-        public async Task<IActionResult>FindFlight (String SeatClass, String AmountPassenger, String DestinationFrom, String DestinationTo, String DepartureDate)
+        public async Task<IActionResult>FindFlight (FindFlightDto request)
         {
-        // public string SeatClass {get; set;} = string.Empty;
-        // public int AmountPassenger { get; set; }
-        // public string DestinationFrom {get; set;} = string.Empty;
-        // public string DestinationTo {get; set;} = string.Empty;
-        // public DateTime DepartureDate {get; set;}
-            FindFlightDto request = new FindFlightDto
-            {
-                SeatClass = SeatClass,
-                AmountPassenger = int.Parse(AmountPassenger),
-                DestinationFrom = DestinationFrom,
-                DestinationTo = DestinationTo,
-                DepartureDate = DateTime.Parse(DepartureDate)
-            };
             var response = await _flightService.FindFlight(request);
             List<GetFlightDto> findFlightDtos = response.Data;
             return View(findFlightDtos);
         }
 
-        
+
+        public async Task<IActionResult> DetailFlights(int id)
+        {
+
+            var response = await _flightService.GetFlightDtoById(id);
+            GetFlightDto getFlightbyId = response.Data;
+            return View(getFlightbyId);
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
